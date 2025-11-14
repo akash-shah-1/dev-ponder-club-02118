0380a97290f4ae6d441706784c4433236a95156e
akash-shah-1 <akash.s@cisinlabs.com>
2025-11-14 21:38:47 +0530

    feat: Add Android support with Capacitor

---
+++ client/android/app/src/main/java/com/devoverflow/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.devoverflow.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devoverflow.app',
+  appName: 'DevOverflow',
+  webDir: 'dist'
+};
+
+export default config;
