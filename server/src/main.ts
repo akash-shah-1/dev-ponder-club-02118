1455db03ea4a0b760ee1e185837e4cff40937716
akash-shah-1 <akash.s@cisinlabs.com>
2025-11-18 18:26:40 +0530

    added client url in cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3002'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3002', 'https://devover-flow.vercel.app'],
     credentials: true,
   });
 
